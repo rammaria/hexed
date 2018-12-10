9ab211becec34294832a1f75df24cc97d998d07e
rammaria <rammariadvincula@gmail.com>
2018-12-10 13:39:58 +0800

    board initialization

--- Hexed.java
+++ Hexed.java
@@ -1,11 +1,14 @@
+
+
 import java.lang.*;
+import java.util.*;
+import java.util.LinkedList;
 import java.util.*;
 
 class Move{
-  private int row, col;
-  private char color;
+  private int row, col, color;
 
-  public Move(int row, int col, char color){
+  public Move(int row, int col, int color){
     this.col = col;
     this.row = row;
     this.color = color;
@@ -19,7 +22,7 @@ class Move{
     this.row = row;
   }
 
-  public void setColor(char color){
+  public void setColor(int color){
     this.color = color;
   }
 
@@ -31,19 +34,21 @@ class Move{
     return row;
   }
 
-  public char getColor(){
+  public int getColor(){
     return color;
   }
 
   @Override
   public String toString(){
-    return ("[" + this.col + ", " + this.row + ", " + this.color + "]");
+    return ("[" + this.col + ", " + this.row + ", " + (color == 'g' ? "green" : "red")+ "]");
   }
 }
 
 public class Hexed {
     private static Scanner kbd = new Scanner(System.in);
-    private int[][] board = new int[7][9];
+    private static int[][] board = new int[7][9];
+    public static final int GREEN = 0; //constant color value assignments
+    public static final int RED = 1;
 
   public static void main(String[] args){
 
@@ -53,29 +58,66 @@ public class Hexed {
       System.out.print("Row: ");
       int initRow = kbd.nextInt();
       System.out.print("Color[r/g]: ");
-      char initColor = kbd.next().charAt(0);
+      char color = kbd.next().charAt(0);
+      int initColor = 1;
 
-      setInitial(initRow, initCol, initColor);
+      if (color == 'r') {
+          initColor = 1;
+      } if (color == 'g') {
+          initColor = 0;
+      }
+      
+      setInitialBoard(initRow, initCol, initColor, board);
 
 
   }
 
-  public static void setInitial(int row, int col, char color){
+  public static int[][] setInitialBoard(int row, int col, int color, int[][] board){
+    int[][] startingBoard = new int[7][9];
 
     switch(color){
-      case 'r':
+      case '0':
+          if(col % 2 == 0){
+            board[col][row] = 0;
+            board[col-1][row-1] = 1;
+            board[col-1][row-2] = 0;
+            board[col][row-2] = 1;
+            board[col+1][row-2] = 0;
+            board[col+1][row-1] = 1;
+          }else{
+            board[col][row] = 0;
+            board[col-1][row] = 1;
+            board[col-1][row-1] = 0;
+            board[col][row-2] = 1;
+            board[col+1][row-1] = 0;
+            board[col+1][row] = 1;
+          }
+          break;
+      case '1':
         if(col % 2 == 0){
-
+          board[col][row] = 1;
+          board[col-1][row-1] = 0;
+          board[col-1][row-2] = 1;
+          board[col][row-2] = 0;
+          board[col+1][row-2] = 1;
+          board[col+1][row-1] = 0;
+        }else{
+          board[col][row] = 1;
+          board[col-1][row] = 0;
+          board[col-1][row-1] = 1;
+          board[col][row-2] = 0;
+          board[col+1][row-1] = 1;
+          board[col+1][row] = 0;
         }
         break;
-      case 'g':
-
-
-        break;
     }
+
+    return startingBoard;
   }
 
-  public LinkedList<Move> getValidMoves(){
+  public LinkedList<Move> getMoves(){
+    LinkedList<Move> validMoves = new LinkedList<>();
 
+    return validMoves;
   }
 }
